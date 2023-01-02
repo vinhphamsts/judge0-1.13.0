564c8d0b0af7f06aa11d9282d39ef2aeaa0e3e03
Vinh Pham Thanh <vinh.pham@saigontechnology.com>
2023-01-02 22:02:51 +0700

    Reorder install steps

--- install.sh
+++ install.sh
@@ -12,9 +12,11 @@ apt-cache policy docker-ce
 
 sudo apt install docker-ce
 
-sudo systemctl status docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
 docker-compose --version
+
+sudo systemctl status docker
